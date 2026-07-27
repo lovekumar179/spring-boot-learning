7a38dd50ac57dfddcec2ea24fb70d9edd2aa824b
Love Kumar <lkamlhi99@gmail.com>
2026-07-27 08:55:03 +0500

    Add loose coupling example

---
+++ learn-couplings/src/looseCoupling/EmailNotificationService.java
@@ -0,0 +1,8 @@
+package looseCoupling;
+
+public class EmailNotificationService implements NotificationService{
+  @Override
+  public void send(String message) {
+    System.out.println("Email: "+message);
+  }
+}
---
+++ learn-couplings/src/looseCoupling/UserService.java
@@ -0,0 +1,14 @@
+package looseCoupling;
+
+public class UserService {
+  NotificationService notificationService;
+
+  public UserService(NotificationService notificationService) {
+    this.notificationService = notificationService;
+  }
+
+  public void notifyUser(String message){
+    notificationService.send("Notification hello");
+  }
+
+}

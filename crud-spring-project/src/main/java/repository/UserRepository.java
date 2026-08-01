0f8a370226a03079a08c27074b35b9d9452d8d03
Love Kumar <lkamlhi99@gmail.com>
2026-08-01 21:34:31 +0500

    Created User repository and some methods of it

---
+++ crud-spring-project/src/main/java/repository/UserRepository.java
@@ -0,0 +1,24 @@
+package repository;
+
+import db.DatabaseConnection;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserRepository {
+  private DatabaseConnection db;
+
+  public UserRepository(DatabaseConnection db) {
+    this.db = db;
+  }
+
+  public List<String> findAll() {
+    return db.getUsers();
+  }
+
+  public void save(String user) {
+    db.addUser(user);
+  }
+
+}

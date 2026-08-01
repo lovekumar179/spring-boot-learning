89b5c15e78fa6ba811b7b1fef98ca3fbc1752ab1
Love Kumar <lkamlhi99@gmail.com>
2026-08-01 21:33:13 +0500

    Created Database layer

---
+++ crud-spring-project/src/main/java/db/DatabaseConnection.java
@@ -0,0 +1,28 @@
+package db;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class DatabaseConnection {
+  private List<String> users;
+
+  public void init() {
+    users = new ArrayList<>();
+    System.out.println("DB Connected (simulated)");
+  }
+
+  public List<String> getUsers() {
+    return users;
+  }
+
+  public void addUser(String user) {
+    users.add(user);
+  }
+
+  public void cleanup(){
+    System.out.println("DB connected (simulated)");
+  }
+}

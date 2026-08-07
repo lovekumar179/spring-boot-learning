d75a911ca50c6a641b18fc2b5b119c852096d13b
Love Kumar <lkamlhi99@gmail.com>
2026-08-07 17:44:08 +0500

    Learnt: about Application Runner in spring boot

---
+++ springBoot-first/src/main/java/com/backend/first/springBootStartup/AppArgsRunner.java
@@ -0,0 +1,13 @@
+package com.backend.first.springBootStartup;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppArgsRunner implements ApplicationRunner {
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+    System.out.println("Options: "+args.getOptionNames());
+  }
+}

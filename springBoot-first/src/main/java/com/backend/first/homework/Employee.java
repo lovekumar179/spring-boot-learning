8bd99aa2026f07c1db9c622cf9e8686f822b9dd5
Love Kumar <lkamlhi99@gmail.com>
2026-08-07 17:49:47 +0500

    DONE: Home work of restAPIs which is employees apis

---
+++ springBoot-first/src/main/java/com/backend/first/homework/Employee.java
@@ -0,0 +1,37 @@
+package com.backend.first.homework;
+
+public class Employee {
+  private int id;
+  private String name;
+  private String role;
+
+  public Employee(int id, String name, String role) {
+    this.id = id;
+    this.name = name;
+    this.role = role;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getRole() {
+    return role;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setRole(String role) {
+    this.role = role;
+  }
+}
---
+++ springBoot-first/src/main/java/com/backend/first/homework/EmployeeController.java
@@ -0,0 +1,52 @@
+package com.backend.first.homework;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+  Map<Integer, Employee> empDb = new HashMap<>();
+
+  @PostMapping
+  public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
+    empDb.putIfAbsent(employee.getId(), employee);
+    return new ResponseEntity<>(employee, HttpStatus.CREATED);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Employee> updateEmployee
+      (@PathVariable int id,
+      @RequestBody Employee employee
+      ){
+    if (!empDb.containsKey(id))
+      return ResponseEntity.notFound().build();
+
+    employee.setId(id);
+    empDb.put(id, employee);
+    return ResponseEntity.ok(employee);
+  }
+
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<String> deleteEmployee(@PathVariable int id){
+    if (!empDb.containsKey(id))
+      return ResponseEntity.notFound().build();
+
+    empDb.remove(id);
+    return ResponseEntity.noContent().build();
+  }
+
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Employee> getEmployee(@PathVariable int id){
+    if (!empDb.containsKey(id))
+      return ResponseEntity.notFound().build();
+    return ResponseEntity.ok(empDb.get(id));
+  }
+
+}

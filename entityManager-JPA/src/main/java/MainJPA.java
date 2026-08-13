98de5bc101baa2d90b2b289b55694f481291e5d0
Love Kumar <lkamlhi99@gmail.com>
2026-08-13 17:14:43 +0500

    Main class for code execution

---
+++ entityManager-JPA/src/main/java/MainJPA.java
@@ -0,0 +1,24 @@
+import jakarta.persistence.EntityManager;
+
+public class MainJPA {
+  public static void main(String[] args) {
+    EntityManager em = JPAUtil.getEntityManager();
+
+    try {
+      UserClassHibernate user = new UserClassHibernate("Alice");
+
+      em.getTransaction().begin();
+      em.persist(user); // save(user)
+      em.getTransaction().commit();
+
+      System.out.println("User saved: "+user.getId());
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      em.close();
+      JPAUtil.close();
+    }
+
+  }
+}

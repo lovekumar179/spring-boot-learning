9639a026d212c2c25a81fc0ae33fe817ec6ede12
Love Kumar <lkamlhi99@gmail.com>
2026-08-14 13:02:33 +0500

    Project files

---
+++ springBoot-second-JPA/src/test/java/com/backend/springDataJPA/SpringBootSecondJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.springDataJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecondJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

da489b39ec398d7b9fe58e8ee6e7bdfaf54b179f
Love Kumar <lkamlhi99@gmail.com>
2026-08-14 13:00:08 +0500

    Learnt: Resposiory class jpa respository and Crud repository

---
+++ springBoot-second-JPA/src/main/java/com/backend/springDataJPA/UserRepository.java
@@ -0,0 +1,8 @@
+package com.backend.springDataJPA;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserClassSpringBoot, Long> {
+}
